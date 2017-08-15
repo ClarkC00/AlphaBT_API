9df8976ebd61d52c0f6eee9e2f7911241b34638f
ClarkC00 <chenxi.neau@gmail.com>
2017-08-15 13:00:26 +0800

    fix bugs

--- SAS_API/AlphaBT_SAS_API.py
+++ SAS_API/AlphaBT_SAS_API.py
@@ -4,7 +4,7 @@ Created on Tue Aug 15 10:44:39 2017
 
 @author: chenxi
 """
-
+import os
 from sas7bdat import SAS7BDAT
 import numpy as np
 import pandas as pd
@@ -20,11 +20,18 @@ def stockNameTrans(stockNum):
         
     return stockCode
 
-infile = r'F:\Quant\AlphaBT\DataBase3\Future\Future_SAS_files\csi500_idx_dwt.sas7bdat'
+infile = r'F:\Quant\AlphaBT\DataBase3\Future\Future_SAS_files\shsz300_idx_dwt.sas7bdat'
 
+outputDir = r'F:\Quant\AlphaBT\DataBase3\Future'
+outfileName = 'if300'
+
+start_date = '2011-01-04'
+end_date = '2017-08-10'
 
 with SAS7BDAT(infile) as f:
     df = f.to_data_frame()
+    
+df['enddt'] = pd.to_datetime(df['enddt'])
 df['stkcd'] = df['stkcd'].map(int).map(stockNameTrans)
 
 df=df.pivot_table(index='enddt',columns='stkcd',values='standard_wt', aggfunc=np.mean) # for duplicate apply mean function
@@ -46,7 +53,17 @@ futureValid = futureValid.join(df.T, how = 'outer')
 
 futureValid = futureValid.T
 
-futureWeights = np.copy(futureValid)
+futureValid = futureValid[start_date : end_date]
 
+futureWeights = futureValid.copy()
+
+futureValid[np.isnan(futureValid)] = 0
 futureValid[futureValid > 0] = 1
+
+
+fileName = os.path.join(outputDir, outfileName+'.csv')
+futureValid.to_csv(fileName, header = True)
+
+fileName = os.path.join(outputDir, outfileName + '_weights' + '.csv')
+futureWeights.to_csv(fileName, header = True)
 
